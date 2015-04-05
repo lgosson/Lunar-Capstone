d79dbeb384f34d527796a24aad780dc399569908
caseydedore <caseydedore@gmail.com>
2015-04-04 18:37:30 -0700

    Added Arbor.js files
    Arbor.js files have been added, which defines the logic necessary for
    graph drawing. Basic starting point js has been added for using Arbor

---
+++ LunarLogicServices/Scripts/Services.js
@@ -0,0 +1,8 @@
+﻿//given our knowledge of js, I figure commenting our code heavily is a good idea for now. 
+//We will want to remove most comments or run an optimizer to reduce file size later on
+
+//self-invoking function
+(function(){
+
+
+})()
