c08a184d21d1816acb93c8ebde0b66f780c94c7b
caseydedore <caseydedore@gmail.com>
2015-04-05 13:42:12 -0700

    Ported over Jesse's initial Arbor test
    The test Jesse did in the Lunar Test project has been replicated on the
    Services view.

---
+++ LunarLogicServices/Controllers/ServicesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LunarLogicServices.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return RedirectToAction("Services");
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
--- LunarLogicServices/Scripts/Services.js
+++ LunarLogicServices/Scripts/Services.js
@@ -1,8 +1,23 @@
-﻿//given our knowledge of js, I figure commenting our code heavily is a good idea for now. 
-//We will want to remove most comments or run an optimizer to reduce file size later on
-
-//self-invoking function
+﻿
 (function(){
 
+    var sys = arbor.ParticleSystem(500, 1000, 1);
+    sys.parameters({ gravity: true });
+    sys.renderer = Renderer("#viewport");
+    var data = {
+        nodes: {
+            stars: { 'color': 'red', 'shape': 'dot', label: 'TITLE HERE', },
+            planet1: { 'color': 'green', 'shape': 'dot', 'label': 'Planet' },
+            planet2: { 'color': 'blue', 'shape': 'dot', 'label': 'Planet' },
+            planet3: { 'color': 'blue', 'shape': 'dot', 'label': 'Planet' },
+            planet4: { 'color': 'blue', 'shape': 'dot', 'label': 'Planet' }
+
+        },
+        edges: {
+            stars: { planet1: {}, planet2: {} },
+            planet1: { planet3: {}, planet4: {} }
+        }
+    };
+    sys.graft(data);
 
 })()
