b2d7cdfdc80501f0fa2e5fe89ae4eb70ee4621fb
jesseaw21 <jessewood21@gmail.com>
2015-04-05 11:14:03 -0700

    Arbor working

---
+++ LunarTests/Lunar/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string star = "HIPPOPTOMAS";
+            ViewBag.Name = star;
+            return View();
+        }
+    }
+}
---
+++ LunarTests/Lunar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lunar.MvcApplication" Language="C#" %>
