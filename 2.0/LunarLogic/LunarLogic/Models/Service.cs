431bc780b8b6ec47905b40d6985a2fbf698fa1d0
lgosson <pudge_gosson21@hotmail.com>
2015-05-19 14:48:31 -0700

    fixed create page, edit page has bug that disable wrong services
    theres a comment detailing the bug, it in the edit post controller

--- 2.0/LunarLogic/LunarLogic/Models/Service.cs
+++ 2.0/LunarLogic/LunarLogic/Models/Service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,6 +15,7 @@ namespace LunarLogic.Models
             Selectable = true;
         }
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
