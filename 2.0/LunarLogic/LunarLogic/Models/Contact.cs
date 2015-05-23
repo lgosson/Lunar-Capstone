39394e67b61b37f932cc30d29b8ab9f430fdab99
jesseaw21 <jessewood21@gmail.com>
2015-05-23 14:15:01 -0700

    Added validation to contact form

--- 2.0/LunarLogic/LunarLogic/Models/Contact.cs
+++ 2.0/LunarLogic/LunarLogic/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,20 @@ namespace LunarLogic.Models
     public class Contact
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(20, ErrorMessage = "First name cannot be longer than 20 characters")]
         public string FirstName { get; set; }
+        [StringLength(20, ErrorMessage = "Last name cannot be longer than 20 characters")]
         public string LastName { get; set; }
+        [StringLength(20)]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                            ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
+        [StringLength(200, ErrorMessage = "Comment must not be larger than 200 characters")]
         public string Comment { get; set; }
         public string Services { get; set; }
     }
