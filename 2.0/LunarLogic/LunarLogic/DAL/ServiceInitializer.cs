2ba8724380f5e3ea973ca0f982552683e7a794f7
lgosson <pudge_gosson21@hotmail.com>
2015-05-31 07:46:02 -0700

    Fixed service icon url strings in Initializer

--- 2.0/LunarLogic/LunarLogic/DAL/ServiceInitializer.cs
+++ 2.0/LunarLogic/LunarLogic/DAL/ServiceInitializer.cs
@@ -11,12 +11,12 @@ namespace LunarLogic.DAL
     {
         protected override void Seed(ServiceContext context)
         {
-            Service s1 = new Service() { ID = 1, Selectable = false, Name = "Lunar Logic", ImageURL = "~/images/serviceicon1.png", Description = "Comes From Server.." };
+            Service s1 = new Service() { ID = 1, Selectable = false, Name = "Lunar Logic", ImageURL = "/images/serviceicon1.png", Description = "Comes From Server.." };
             Service s2 = new Service()
             {
                 ID = 2,
                 Name = "Service02",
-                ImageURL = "~/images/serviceicoe1.png",
+                ImageURL = "/images/serviceicon1.png",
                 Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a"  };
             Service s3 = new Service() { ID = 3, Name = "Service03", ImageURL = "/images/serviceicon1.png", Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a" };
             Service s4 = new Service() { ID = 4, Name = "Service04", ImageURL = "/images/serviceicon2.png", Description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui" };
